c01ae497333a8cc0758952a49c3d3d57bb753912
Andrew.K <akirillychev@yahoo.com>
2020-10-28 01:20:37 -0400

    new heroku

---
+++ server.js
@@ -0,0 +1,20 @@
+// npm packages that help bring our app's functionality to life
+const express = require('express');
+
+
+// Initialize express App
+const app = express();
+var PORT = process.env.PORT || 3080; 
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('./public'));
+
+// Require routes file
+require("./Routes/apiRoutes")(app);
+require("./Routes/htmleRoutes")(app);
+
+// Helps let us know that the port is listening
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT);
+}); 
